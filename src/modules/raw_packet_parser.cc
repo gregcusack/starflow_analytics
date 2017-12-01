9008e1a6381517970d64e530556e6126dd42a8ff
Oliver Michel <oliver.michel@editum.de>
2017-12-01 14:23:24 -0700

    make raw packet parser compatible with unique_ptr in raw_packet

--- src/modules/raw_packet_parser.cc
+++ src/modules/raw_packet_parser.cc
@@ -50,24 +50,24 @@ bool starflow::modules::RawPacketParser::_parse_packet(const types::RawPacket& r
 	struct udphdr*       udp = nullptr;
 
 	if (_outer_header == outer_header_type::eth) {
-		eth = (ether_header*) raw_packet.pl;
+		eth = (ether_header*) raw_packet.pl.get();
 		pkt_offset += sizeof(struct ether_header);
 
 		if (ntohs(eth->ether_type) != ETHERTYPE_IP)
 			return false;
 	}
 
-	ip = (struct ip*) (raw_packet.pl + pkt_offset);
+	ip = (struct ip*) (raw_packet.pl.get() + pkt_offset);
 
 	packet.features.ip_ttl = ip->ip_ttl;
 
 	pkt_offset += sizeof(struct ip);
 
 	if (ip->ip_p == IPPROTO_UDP) {
-		udp = (struct udphdr*) (raw_packet.pl + pkt_offset);
+		udp = (struct udphdr*) (raw_packet.pl.get() + pkt_offset);
 		key = {ip->ip_p, ip->ip_src, ip->ip_dst, ntohs(udp->uh_sport), ntohs(udp->uh_dport)};
 	} else if (ip->ip_p == IPPROTO_TCP) {
-		tcp = (struct tcphdr*) (raw_packet.pl + pkt_offset);
+		tcp = (struct tcphdr*) (raw_packet.pl.get() + pkt_offset);
 		key = {ip->ip_p, ip->ip_src, ip->ip_dst, ntohs(tcp->th_sport), ntohs(tcp->th_dport)};
 		packet.features.tcp_flags = types::Features::tcp_flags_t(tcp->th_flags);
 	} else return false;
